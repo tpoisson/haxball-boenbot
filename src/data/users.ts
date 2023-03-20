c33e1a766cc1c265b11e8036e8e80fbd6e04b358
Thomas POISSON <tfished@gmail.com>
2023-03-20 18:27:33 +0100

    feat(user): Add super admin

--- src/data/users.ts
+++ src/data/users.ts
@@ -4,6 +4,7 @@ export const registeredUsers: RegisteredUser[] = [
   {
     id: "fish",
     name: "Fish",
+    superAdmin: true,
     publicIds: [
       "OKlLNmjrZvzihrET0nt-y1QIS9T9iK0NsQQAy3Wqe0s",
       "DvYez5QUWhoEW5Tn3vUrXthpuZkr5Dz19_1MNbGtETs",
@@ -14,42 +15,49 @@ export const registeredUsers: RegisteredUser[] = [
   {
     id: "dofunk",
     name: "Dofunk",
+    superAdmin: false,
     publicIds: ["r2MhTPxiTWSKNwg7DNTcTHEV2c6FbdPN0qXJ5ICtsjA"],
     greetings: ["Le petit cowboy vient d'entrer dans la room ! 🤠", "Nicolas ? Tu es réveillé ?! 💤"],
   },
   {
     id: "val",
     name: "Hunt3ur",
+    superAdmin: false,
     publicIds: ["ymOq0imV3LTinY6mEACkG6utlTjcZXLGQLQzpaPeXjA"],
     greetings: ["Rick Hunter est dans la place 🦊", "Tiens, y'a VAL !"],
   },
   {
     id: "froot",
     name: "Franfoot",
+    superAdmin: false,
     publicIds: ["hnGi__rCaFlCEulyO6AFgfZZJ1TIBBwJBJOjQXAJ4mw"],
     greetings: ["Le boule le plus charpenté de l'histoire est parmi nous ! 🍑💪", "Belle intuition, FOUTRE!"],
   },
   {
     id: "pat",
     name: "Pat",
+    superAdmin: false,
     publicIds: [],
     greetings: ["Paaat' ! 🍹💃🏽🇧🇷", "OMG Paaat' ! 🍹💃🏽🇧🇷", "PAT' LA CHATTE ! 🍹💃🏽🇧🇷"],
   },
   {
     id: "laura",
     name: "Laura",
+    superAdmin: false,
     publicIds: ["wWh2uo9A8zskwre1AY1oi1BRKmfTX8NowyN8vQcPjNA"],
     greetings: ["🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦🥦"],
   },
   {
     id: "vv",
     name: "the wolf",
+    superAdmin: false,
     publicIds: ["B0WriS404UO1gLBStVtizVVlDeqqJSa4W9QYzhuwdYQ"],
     greetings: ["Un membre de la Drama Team s'est connecté 🐺 !", "Aaahooouuuuuuuuu 🐺"],
   },
   {
     id: "thompoul",
     name: "thompoul",
+    superAdmin: false,
     publicIds: ["ouwBCSX9sRDL1T6irDgnyLmDfTDV6QQ5f37A_-6S2nM"],
     greetings: ["Bonjour Thomas POGNON ! 💵🤑₿", "L'autre membre de la Drama Team s'est connecté 🐴 !"],
   },
