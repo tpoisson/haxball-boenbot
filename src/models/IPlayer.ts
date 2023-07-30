a25904c722e40653d08d723a00d21ef0587b7248
Thomas POISSON <thomas.poisson.partner@decathlon.com>
2023-07-30 19:07:11 +0200

    refactor(blink): Make idle player a plugin

--- src/models/IPlayer.ts
+++ src/models/IPlayer.ts
@@ -1,6 +1,3 @@
-export interface IPlayerActivity {
-  date: Date;
-}
 export interface IPlayerStats {
   playerId: string;
   nbGoals: number;
---
+++ src/plugins/idle-player.ts
@@ -0,0 +1,47 @@
+import IChatCommand from "../models/IChatCommand";
+import RoomPlugin from "../room/room-plugin";
+
+interface IPlayerActivity {
+  date: Date;
+}
+
+export class IdlePlayerPlugin extends RoomPlugin {
+  private readonly playerLastActivities = new Map<number, IPlayerActivity>();
+  private idleTimeout = 7 * 1000;
+
+  public onGameTick(): void {
+    for (const [playerId, playerData] of this.playerLastActivities) {
+      if (new Date().getTime() - playerData.date.getTime() > this.idleTimeout) {
+        const player = this.room.getPlayer(playerId);
+        if (player && player.team !== 0) {
+          this.playerLastActivities.delete(playerId);
+          // this.room.setPlayerTeam(playerId, 0);
+          this.room.sendAnnouncement(`😴 ${player.name} is ronpiching`);
+        }
+      }
+    }
+  }
+
+  public onPlayerActivity(player: PlayerObject): void {
+    if (!this.playerLastActivities.has(player.id)) {
+      this.playerLastActivities.set(player.id, {
+        date: new Date(),
+      });
+    }
+    this.playerLastActivities.get(player.id)!.date = new Date();
+  }
+  public onPositionsReset(): void {
+    this.playerLastActivities.clear();
+  }
+  public onGamePause(byPlayer: PlayerObject): void {
+    this.playerLastActivities.clear();
+  }
+
+  public onGameUnpause(byPlayer: PlayerObject): void {
+    this.playerLastActivities.clear();
+  }
+
+  getChatsCommands(): IChatCommand[] {
+    return [];
+  }
+}
