53fadd4d3bdabec6f2bb232eccfdc44fe306b3f0
Thomas POISSON <tfished@gmail.com>
2023-07-31 13:38:34 +0200

    refactor(ballpossession): Make ball-possession a plugin

--- src/models/ICurrentGame.ts
+++ src/models/ICurrentGame.ts
@@ -6,5 +6,4 @@ export interface ICurrentGame {
   scoring: { scorer: PlayerObject; time: number; ownGoal: boolean; assist?: PlayerObject }[];
   startTime: Date;
   endTime?: Date;
-  // possessions: { player: PlayerObject; ticks: number }[];
 }
